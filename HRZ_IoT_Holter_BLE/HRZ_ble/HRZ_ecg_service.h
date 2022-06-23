b49ddf557e85eb3a975ababddb5dd7bbea4556ec
Jose Granados <jose@grana2.co>
2022-06-22 20:56:05 -0500

    Change packet length

--- HRZ_IoT_Holter_BLE/HRZ_ble/HRZ_ecg_service.h
+++ HRZ_IoT_Holter_BLE/HRZ_ble/HRZ_ecg_service.h
@@ -26,7 +26,7 @@ extern "C" {
 #define BLE_UUID_ECG_CHANNEL_7              0x8177  /**< ECG Channel 7 characteristic UUID */
 #define BLE_UUID_ECG_CHANNEL_8              0x8178  /**< ECG Channel 8 characteristic UUID */
 #define HRZ_CHANNEL_LEN                     3       /**< Number of bytes per sample per channel. 3 for non filtered data, 4 for filtered */
-#define HRZ_SAMPLES_PER_PACKET              21      /**< Number of samples per BLE packet. 28 for non filtered data, 21 for filtered */
+#define HRZ_SAMPLES_PER_PACKET              28      /**< Number of samples per BLE packet. Max 28 without crashing */
 #define HRZ_ECGS_MAX_BUFFER_SIZE            (HRZ_CHANNEL_LEN * HRZ_SAMPLES_PER_PACKET)      /**< Total number of bytes per channel per BLE send. Usually 84 */
 
 
